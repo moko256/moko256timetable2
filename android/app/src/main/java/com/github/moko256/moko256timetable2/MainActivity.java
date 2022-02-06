58f74394d3972ffbd3afb40fcaaa51ba01a049bd
moko256 <koutaro.mo@gmail.com>
2022-02-07 06:18:45 +0900

    chore: update android template

---
+++ android/app/src/main/java/com/github/moko256/moko256timetable2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.github.moko256.moko256timetable2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/moko256timetable2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moko256timetable2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
